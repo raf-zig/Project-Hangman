92ef47f6bc4838fc20a59505d3263955e40d8281
raf-zig <ziravin@yandex.ru>
2023-02-21 22:59:38 +0500

    Add a call save_game method

--- lib/game.rb
+++ lib/game.rb
@@ -21,24 +21,27 @@ def user_choice
 end
 
 def check_result(secret_word, bad_letters, guessed_word, user_choice)
-  user_letter = user_choice
   secret_word.each_with_index do |letter, index|
-    if letter == user_letter
-      guessed_word[index] = user_letter
+    if letter == user_choice
+      guessed_word[index] = user_choice
     end
   end
-  unless secret_word.include?(user_letter)
-    bad_letters << user_letter unless bad_letters.include?(user_letter)
+  unless secret_word.include?(user_choice)
+    bad_letters << user_choice unless bad_letters.include?(user_choice)
   end
 end
 
 i = 0
 while i < 4 do
   i += 1
+  puts 'Would you like to save the game? y/n'
+  save_game = gets.downcase.chomp
+  if save_game == 'y'
+    save_game(secret_word, guessed_word, i, bad_letters)
+    break
+  end
   puts 'Enter word:'
   check_result(secret_word, bad_letters, guessed_word, user_choice)
-  puts 'Would you like to save the game? y/n'
-  user_letter = gets.downcase.chomp
   puts guessed_word.join
   puts "Errors - #{bad_letters.join}"
   puts "There are still attempts left - #{4-i}"
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,25 +1,23 @@
 require 'yaml'
 
-def save_game(secret_word, guessed_word, score, bad_letters, good_letters)
-  puts 'Do you want to save the game? (y/n)'
-  data = [secret_word, guessed_word, score, bad_letters]
+def save_game(secret_word, guessed_word, i, bad_letters)
+  data = [secret_word, guessed_word, i, bad_letters]
   File.open('test1.yaml', 'w') do |file|
     file.puts YAML.dump data
   end
 end
-#save_game(['k', 'f'], ['g', 'r'], 5, ['w', 'v'], ['s', 'q'])
 
 def continue_game
   data = YAML.load File.read('test1.yaml') 
-  secret_word, guessed_word, score, bad_letters = data[0], data[1], data[2], data[3]
+  secret_word, guessed_word, i, bad_letters = data[0], data[1], data[2], data[3]
   print secret_word
   print guessed_word
-  print score
+  print i
   print bad_letters
-  print good_letters
+ 
 end
 
-#continue_game
+
 
 
 
