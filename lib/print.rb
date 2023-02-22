eaa026a2fcbe164575c6b33439fdd69a0efb497a
raf-zig <ziravin@yandex.ru>
2023-02-22 21:59:57 +0500

    Add print.rb

---
+++ lib/print.rb
@@ -0,0 +1,54 @@
+def print(i)
+  case i
+  when 1
+    puts '
+     |
+    ( )'
+  when 2 
+    puts '
+     |
+    ( )
+     |'
+  when 3 
+    puts '
+     |
+    ( )
+     |
+      \\'
+  when 4 
+    puts '
+     |
+    ( )
+     |
+    / \\'
+  when 5 
+    puts '
+     |
+    ( )
+     |
+    /|\
+     |'
+  when 6 
+    puts '
+     |
+    ( )
+     |
+    /|\
+     |
+      \\'
+  when 7 
+   puts ' 
+     |
+    ( )
+     |
+    /|\
+     |
+    / \\'
+  end
+end
+
+
+
+
+
+
